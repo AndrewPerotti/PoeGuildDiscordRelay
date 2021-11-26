1d5f9bd6f32d9be5431185afab7d32146a58425e
Perotti28 <51537156+Perotti28@users.noreply.github.com>
2021-11-26 10:04:18 -0800

    Update PoeGuildDiscordRelay.py

--- PoeGuildDiscordRelay.py
+++ PoeGuildDiscordRelay.py
@@ -1,6 +1,4 @@
-"""A program which runs a discord bot while a copy of PathOfExile is running which reads its Client.txt file and pastes new guild messages into a given channel that the bot is invited to."""
 import discord 
-import os
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 import time
@@ -9,7 +7,7 @@ import pyperclip
 client = discord.Client()
 tk.Tk().withdraw() 
 clienttxt = askopenfilename()
-
+guild_var = '&<something> ' #if you don't have a tag you'll just use '&' as far as i know, otherwise '&<something> ' is for the guild tag 'something' and the the space is included for formatting purposes of the output
 
 #define function for reading
 def return_guild_message(clientfile):
@@ -18,17 +16,21 @@ def return_guild_message(clientfile):
     f.seek(0,2)
     while True:
         line = f.readline()
-        if "&" in line:
+        if guild_var in line:
             line.encode(encoding='UTF-8', errors='replace')
-            pyperclip.copy(line[52:])
-            return pyperclip.paste()
+            pyperclip.copy(line)
+            formatted_string = pyperclip.paste()
+            temp_string_list = formatted_string.split(guild_var)
+            final_string = temp_string_list[1]
+            return final_string
         else:
             continue
 
+#When the bot is up and running the terminal will have a print out, and then it runs a whle loop which checks against a global variable which our python script creates while reading Client.txt file.            
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    channel = client.get_channel(#channel#)
+    channel = client.get_channel("""Your channel's Id goes here, sorry I did not include the code to obtain that you'll have to figure that out""")
     last_message = None
     while True:
         outputG = return_guild_message(clienttxt)
@@ -38,14 +40,6 @@ async def on_ready():
         else:
             time.sleep(0.1)
             continue
-
-
-
-
-
-
-
-
-client.run('#bot token')
-
-
+            
+#this runs the bot, need to pass in your bots token
+client.run(' """Your bot token goes here, with the quotes""" ')
