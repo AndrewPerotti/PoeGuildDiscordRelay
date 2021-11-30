93a9767cd12bf776c938a3a09c52e747968008eb
Perotti28 <51537156+Perotti28@users.noreply.github.com>
2021-11-30 12:00:42 -0800

    Update PoeGuildDiscordRelay.py
    Revamped to a more lightweight, less non programmer user friendly build.

--- PoeGuildDiscordRelay.py
+++ PoeGuildDiscordRelay.py
@@ -4,45 +4,25 @@ from tkinter.filedialog import askopenfilename
 import time
 
 client = discord.Client()
-tk.Tk().withdraw() 
-clienttxt = askopenfilename()
-guild_var = '&<Jë§µs†> '
+clienttxt = 'C:/Program Files (x86)/Grinding Gear Games/Path of Exile/logs/Client.txt' #This is the path to your Client.txt.
+guild_var = '&<guild> ' #this is not only your guild tag but is also used with the split method to format output, the space after the > is required
 
-#define function for reading
-def return_guild_message(clientfile):
-    """A function that prints out the guild chat entries of client.txt."""
-    with open(clientfile, encoding='utf-8') as f:
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    channel = client.get_channel(#here you need to input a channel id#, you'll need to figure that out on your own)
+    last_message = None
+    with open(clienttxt, encoding = 'utf-8') as f:
         f.seek(0,2)
         while True:
             line = f.readline()
             if guild_var in line:
                 temp_string_list = line.split(guild_var)
                 final_string = temp_string_list[1]
-                return final_string
+                await channel.send(final_string)
+                print(final_string)
             else:
+                time.sleep(0.1)
                 continue
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    channel = client.get_channel("""Channel ID""")
-    last_message = None
-    while True:
-        outputG = return_guild_message(clienttxt)
-        if outputG != last_message:
-            last_message = outputG
-            await channel.send(outputG)
-        else:
-            time.sleep(0.1)
-            continue
-
-
-
-
-
-
-
-
-client.run('"""bot token"""')
-
-
+client.run('#this will be the token of your bot, and you need to keep the quotes')
